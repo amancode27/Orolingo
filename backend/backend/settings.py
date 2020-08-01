ae2ef6e5e82e2fcd2287c22e07ade76988cd1762
Aman Agrawal <amanagrawal2702@gmail.com>
2020-08-01 17:48:00 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'orolingo',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123456',
         'HOST': '',
         'PORT': '',
         'OPTIONS': {
