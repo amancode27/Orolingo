2b66c3fa134bcee6b7b0e761f852ed9f5a6fcb28
Aman Agrawal <amanagrawal2702@gmail.com>
2020-08-03 15:15:58 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -207,5 +207,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'amanagrawal2702@gmail.com'
-EMAIL_HOST_PASSWORD = 'aman2001$'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
