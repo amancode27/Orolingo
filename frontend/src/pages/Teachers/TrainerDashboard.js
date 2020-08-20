345a1c6d5aa7bc8b7ed79a88c9d0817c16930423
Awais_Bro <AWAISAKHTAR270@IITKGP.AC.IN>
2020-08-20 22:08:20 +0530

    Trainer Dashboard UI added, issues in functionality

--- frontend/src/pages/Teachers/TrainerDashboard.js
+++ frontend/src/pages/Teachers/TrainerDashboard.js
@@ -11,41 +11,92 @@ import {
   Button,
   Row,
   Col,
+  Container,
+  
 } from "reactstrap";
+import DropDown from "../Students/Dropdown.js";
+import { Chip, Link } from "@material-ui/core";
 
 const TrainerDashboard = (props) => {
   const [languages, setLanguages] = useState([]);
-
+  //const [languagestoteach,setLanguagesToTeach] = useState({});
+  const [availabletoteach, setavailabletoteach] = useState({"English":"English", "Spanish":"Spanish"});
+  // const addToTeachLanguage = (key,value) =>{
+  //   setLanguagesToTeach((prevState) =>{return {...prevState,[key]:value}});
+  // }
   useEffect(() => {
     axios
       .get(`${basename}/api/language/?trainers=${props.userId}`)
       .then((res) => {
         setLanguages(res.data.objects);
       });
+    //  axios
+    //  .get(`${basename}/api/language_trainer/?trainers=${props.userId}`)
+    //  .then((res) => {
+    //     const languagestoteach = res.data.languages_to_teach;
+    //     languagestoteach.forEach((e) => {
+    //       axios.get(`${basename}${e}`).then((res) =>
+    //       setLanguagesToTeach((prev) => {
+    //         return { ...prev, [res.data.name]: e };
+    //       })
+    //     );
+    //     })
+    //  }); 
   });
 
   return (
     <div>
-    <Card body outline color="success">
+      <Container>
+    <Card body>
       <CardBody>
-        <CardTitle>Languages you teach</CardTitle>
-        <CardText>
+        <Row>
+        <CardTitle>Languages you teach  : </CardTitle>
           <Row>
+            <Col className="ml-3">
             {languages.map((element) =>
-              <div>
-              <Col md = "12" sm = "12"> 
-              <Card body>
-              <CardTitle>{element.name}</CardTitle>
-              <CardText>Details of the course .... </CardText>
-              <Button color="info">Assignments </Button> 
-              </Card>
-              </Col>    
-              </div>
+              <Chip label={element.name} clickable color="primary" className="mr-3"></Chip>
             )}
+            </Col>
           </Row>
-        </CardText>
+        </Row> 
       </CardBody>
     </Card>
+    <Row >
+      <Col md = "4">
+    <Card body>
+      <CardTitle className="text-center">Profile (Trainer) </CardTitle>
+      <img width="100%" src="" alt="Profile photo" />
+      <Card className="text-center">
+        <CardBody>
+          <Row>
+            <CardTitle className="text-left mr-3 ml-3">Want to teach</CardTitle>
+            <CardText>
+              <Row>
+                <div>
+                  <Col>
+                  <Link to="">
+                    <Chip label = "Demo"></Chip>
+                  </Link>
+                  </Col>
+                </div>
+              </Row>
+            </CardText>
+          </Row>
+        </CardBody>
+        <DropDown availLanguages = {availabletoteach}></DropDown>
+      </Card>
+    </Card>
+    </Col>
+    <Col md="8">
+      <Card>
+        <CardBody>
+          <CardTitle className = "text-center">Discussion Forum</CardTitle>
+
+        </CardBody>
+      </Card>
+    </Col>
+    </Row>
+    </Container> 
     </div>
   );
 };
