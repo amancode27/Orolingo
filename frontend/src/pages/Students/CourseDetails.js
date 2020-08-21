4412aa3cf03399f873595c8cd35033f9fd6b2f69
Anindya Sikdar <58126719+Anindya-2000@users.noreply.github.com>
2020-08-21 09:29:56 +0530

    course details updated

--- frontend/src/pages/Students/CourseDetails.js
+++ frontend/src/pages/Students/CourseDetails.js
@@ -2,8 +2,19 @@ import React from 'react';
 import {useState,setState} from 'react';
 import '../style/CourseDetails.css';
 import Reviews from './Reviews';
+import { Button } from 'reactstrap';
 const CourseDetails = (props) =>{
-    const [courseDetails,setCourseDetals] = useState({});
+    const buttonStyle = {
+        width:"100px",
+        fontSize:"15px",
+        fontFamily: "sans-serif",
+        height:"40px",
+        borderRadius:"10px",
+        left:"80%",
+        top:"89%",
+        position:"absolute"
+      };
+    const [courseDetails,setCourseDetals] = useState({"courseName":"English"});
     const [teacherDetails,setTeacherDetails] = useState({});
     return(
         <div>
@@ -19,7 +30,19 @@ const CourseDetails = (props) =>{
                         <Reviews/>
                     </div>
                     <div id="course-details">
-                        
+                        <div id="course-desc">
+                            <h1 style={{textAlign:"center",paddingTop:"30px",textDecoration:"underline"}}>Course Details</h1>
+                            <ul id="about">
+                                <li>Course Name : </li>
+                                <li>Course Duration : </li>
+                                <li>Prerequisites : </li>
+                                <li>Brief Discription :</li>
+                            </ul>
+                            <Button style ={buttonStyle} color="primary" size="lg">Buy</Button>
+                        </div>
+                        <div id="course-video">
+                            
+                        </div>
                     </div>
                 </div>
             </div>
