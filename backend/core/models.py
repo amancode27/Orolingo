3d80873936f608332ca9248d670995709434d601
Awais_Bro <AWAISAKHTAR270@IITKGP.AC.IN>
2020-08-22 22:40:57 +0530

    Discussion forum backend added

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
+from django.utils.text import Truncator
+from django.utils.timezone import now
+
+
 
 
 class User(AbstractUser):
@@ -116,3 +121,59 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+
+class Forum(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            # Newly created object, so set slug
+            self.slug = slugify(self.name)
+        super(Forum, self).save(*args, **kwargs)
+
+class Thread(models.Model):
+    name = models.CharField(max_length=255)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE, related_name='threads')
+    pinned = models.BooleanField(default=False)
+    content = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='threads')
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_activity = models.DateTimeField(default=now)
+
+    class Meta:
+        ordering = [
+            '-pinned',
+            '-last_activity'
+        ]
+
+    def __str__(self):
+        truncated_name = Truncator(self.name)
+        return truncated_name.chars(30)
+
+class Post(models.Model):
+    """ Model to represent the post in a thread """
+    content = models.TextField()
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        truncated_content = Truncator(self.content)
+        return truncated_content.chars(30)
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,8 +14,23 @@ urlpatterns = [
     path('<int:pk>/', include(router.urls)), 
     path('oauth/login/', SocialLoginView.as_view()),
     path('oauth/', include('rest_framework_social_oauth2.urls')),
+    path('api/forum/', ForumListAPIView.as_view(), name='forum-list'),
+    path('api/forum/create/', ForumCreateAPIView.as_view(), name='forum-create'),
+    path('api/forum/<slug:slug>/', ForumDetailAPIView.as_view(), name='forum-detail'),
+    path('api/forum/<slug:slug>/edit/', ForumUpdateAPIView.as_view(), name='forum-update'),
+    path('api/forum/<slug:slug>/delete/', ForumDeleteAPIView.as_view(), name='forum-delete'),
+    path('api/post/', PostListAPIView.as_view(), name='post-list'),
+    path('api/post/create/', PostCreateAPIView.as_view(), name='post-create'),
+    path('api/post/<int:pk>/', PostDetailAPIView.as_view(), name='post-detail'),
+    path('api/post/<int:pk>/edit/', PostUpdateAPIView.as_view(), name='post-update'),
+    path('api/post/<int:pk>/delete/', PostDeleteAPIView.as_view(), name='post-delete'),
+     path('api/thread/', ThreadListAPIView.as_view(), name='user-list'),
+    path('api/thread/create/', ThreadCreateAPIView.as_view(), name='thread-create'),
+    path('api/thread/<int:pk>/', ThreadDetailAPIView.as_view(), name='thread-detail'),
+    path('api/thread/<int:pk>/edit/', ThreadUpdateAPIView.as_view(), name='thread-update'),
+    path('api/thread/<int:pk>/delete/', ThreadDeleteAPIView.as_view(), name='thread-delete'),
 ]
-
+  
 # '''
 # 1 - Submit email form                         //PasswordResetView.as_view()
 # 2 - Email sent success message                //PasswordResetDoneView.as_view()
