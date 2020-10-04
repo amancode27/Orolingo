62013c4018b2dbcf5b8d0be14fe29a2ff7969943
Anindya Sikdar <58126719+Anindya-2000@users.noreply.github.com>
2020-10-04 22:40:32 +0530

    Form validation included

--- frontend/src/pages/Teachers/CreateCourse.js
+++ frontend/src/pages/Teachers/CreateCourse.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import basename from "../Home/basename.js";
 import axios from "axios";
-import { Col,Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Col,Button, Form, FormGroup, Label, Input, FormText, Alert } from 'reactstrap';
 import '../style/CreateCourse.css';
 import { Redirect } from "react-router";
 import useFullPageLoader from '../../Components/FullPageLoader/useFullPageLoader.js';
@@ -11,6 +11,24 @@ const CreateCourse = (props) =>{
     const [redirect,setRedirect] = useState(false);
     const [languages,setLanguages] = useState([]);
     const [loader,showLoader,hideLoader] = useFullPageLoader();
+    const [amount_err,setAmount_err] = useState(false);
+    const [name_err,setName_err] = useState(false);
+    const [date_err,setDate_err] = useState(false);
+    const [description_err,setDescription_err] = useState(false)
+    const character_set ={
+        '!':1,
+        '@':1,
+        '#':1,
+        '&':1,
+        '(':1,
+        ')':1,
+        '"':1,
+        "'":1,
+        ' ':1,
+        '-':1
+    }
+
+    const error_string = "Only allowed character set is { a-z , A-Z , 0-9 , ! , @ , # , ( , ) , ' , - " +', " }!'; 
 
     useEffect(()=>{
         axios.get(`${basename}/api/language`)
@@ -19,7 +37,6 @@ const CreateCourse = (props) =>{
         })
     },[props])
     
-    console.log(languages);
 
     const changeField = (e) =>{
         const field = e.target.name;
@@ -27,10 +44,69 @@ const CreateCourse = (props) =>{
         setCourseDetails(prev=>{
             return {...prev,[field]:value};
         })
+
+        if(e.target.name == "name"){
+            let name = e.target.value;
+            if(name)
+            {
+                let f=false;
+                for(let i=0;i<name.length;++i){
+                    if(!((name[i]>='a'&&name[i]<='z')||(name[i]>='A'&&name[i]<='Z')||(name[i]>='0'&&name[i]<='9')||character_set[name[i]])){
+                        f=true;
+                        break;
+                    }
+                }
+                if(f)
+                    setName_err(true);
+                else
+                    setName_err(false);
+            }
+        }
+        if(e.target.name == "description"){
+            let description = e.target.value;
+            if(description)
+            {
+                let f=false;
+                for(let i=0;i<description.length;++i){
+                    if(!((description[i]>='a'&&description[i]<='z')||(description[i]>='A'&&description[i]<='Z')||(description[i]>='0'&&description[i]<='9')||description[i]==','||character_set[description[i]])){
+                        f=true;
+                        break;
+                    }
+                }
+                if(f)
+                    setDescription_err(true);
+                else
+                    setDescription_err(false);
+            }
+        }
+        if(e.target.name == 'cost'){
+            let amount = e.target.value ;
+            if(isNaN(amount)&&amount){
+                setAmount_err(true);
+            }
+            else{
+                setAmount_err(false);
+            }
+        }
+        let start_date = document.getElementById("startDate").value;
+        let end_date = document.getElementById("endDate").value;
+        start_date = Date.parse(start_date);
+        end_date = Date.parse(end_date);
+
+        if(start_date&&end_date&&(end_date<start_date)){
+             setDate_err(true);
+        }
+        else
+            setDate_err(false);
+        
+
     }
-    const submitForm = () =>{
-        showLoader();
+    const submitForm = (e) =>{
+        // showLoader();
+        e.preventDefault();
         const user_id = props.userId;
+        if(amount_err||name_err||description_err||date_err)
+            return;
         axios.get(`${basename}/api/trainer/?user=${user_id}`)
              .then(res1=>{
                  hideLoader();
@@ -71,38 +147,67 @@ const CreateCourse = (props) =>{
              })
         
     }
+    let err1,err2,err3,err4;
+    if(amount_err){
+        err1 = (
+            <Alert color="danger" style={{"marginTop":"20px"}}>Please enter valid amount!</Alert>
+        )
+    }
+    else{
+        err1 = (<div></div>)
+    }
+
+    if(name_err){
+        err2 = (
+            <Alert color="danger" style={{"marginTop":"20px"}}>{error_string}</Alert>
+        )
+    }
+    else{
+        err2 = (<div></div>)
+    }
+
+    if(description_err){
+        err3 = (
+        <Alert color="danger" style={{"marginTop":"20px"}}>{error_string}</Alert>
+        )
+    }
+    else{
+        err3 = (<div></div>)
+    }
+
+    if(date_err){
+        err4 = (
+            <Alert color="danger" style={{"marginTop":"20px"}}>Start Date can't be before End Date!</Alert>
+        )
+    }
+    else
+        err4 = (<div></div>);
+
     if(redirect){
         return <Redirect to={`/dashboard`} />
     }
     return (
         <div className='formcss'>
-            <Form>
+            <Form onSubmit={submitForm}> 
                 <FormGroup>
                     <Label for="courseName">Course Name</Label>
-                    <Input type="text" name="name" id="courseName" placeholder="Enter a course name" bsSize="lg" onChange={changeField}/>
+                    <Input type="text" name="name" id="courseName" placeholder="Enter a course name" bsSize="lg" onChange={changeField} required/>
+                    {err2}
                 </FormGroup>
                 <FormGroup>
                     
                     <Label for="language">Language</Label>
-                    
-                    <Input 
-                        type="text" 
-                        name="language" 
-                        id="language" 
-                        placeholder="Enter the language" 
-                        size="lg" 
-                        list="languagename" 
-                        onChange={changeField}
-                    />
-                    {/* <select 
+                
+                    <Input type="select" 
                         onChange={changeField}
                         name="language" 
-                        value="language"
+                        value={courseDetails['language']}
+                        size="lg"
                     >
-                    {languages.map(e=>{
-                        <option value={e.name}>{e.name}</option>
-                    })}        
-                    </select>  */}
+                        {languages.map(e=>
+                            (<option value={e.name}>{e.name}</option>)
+                        )}        
+                    </Input> 
                 </FormGroup>
                 <FormGroup>
                     <Label for="startDate">Start Date</Label>
@@ -113,6 +218,7 @@ const CreateCourse = (props) =>{
                     placeholder="date placeholder"
                     size="lg"
                     onChange={changeField}
+                    required
                     />
                 </FormGroup>
                 <FormGroup>
@@ -124,19 +230,24 @@ const CreateCourse = (props) =>{
                     placeholder="date placeholder"
                     size="lg"
                     onChange={changeField}
+                    required
                     />
+                    {err4}
                 </FormGroup>
                 <FormGroup>
                     <Label for="cost">Purchase Amount</Label>
-                    <Input type="text" name="cost" id="cost" placeholder="Enter the purchase amount" size="lg" onChange={changeField}/>
+                    <Input type="text" name="cost" id="cost" placeholder="Enter the purchase amount" size="lg" onChange={changeField} required/>
+                    {err1}
                 </FormGroup>
                 <FormGroup>
                     <Label for="description">Course Description</Label>
-                    <Input type="textarea" name="description" id="description" bsSize="lg" onChange={changeField}/>
+                    <Input type="textarea" name="description" id="description" bsSize="lg" onChange={changeField} required/>
+
+                    {err3}
                 </FormGroup>
-                <Button size="lg" onClick={submitForm}>Submit</Button>
+                <Button size="lg" type="submit" >Submit</Button>
             </Form>
-            {loader}
+            {/* {loader} */}
         </div>
     );
 }
--- frontend/src/pages/Teachers/EditCourse.js
+++ frontend/src/pages/Teachers/EditCourse.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import basename from "../Home/basename.js";
 import axios from "axios";
-import { Col,Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Col,Button, Form, FormGroup, Label, Input, FormText ,Alert} from 'reactstrap';
 import '../style/CreateCourse.css';
 import { Redirect } from "react-router";
 import useFullPageLoader from '../../Components/FullPageLoader/useFullPageLoader.js';
@@ -10,12 +10,93 @@ import useFullPageLoader from '../../Components/FullPageLoader/useFullPageLoader
 const CreateCourse = (props) =>{
     const [courseDetails,setCourseDetails] = useState({});
     const [redirect,setRedirect] = useState(false);
+    const [languages,setLanguages] = useState([]);
+    const [amount_err,setAmount_err] = useState(false);
+    const [name_err,setName_err] = useState(false);
+    const [date_err,setDate_err] = useState(false);
+    const [description_err,setDescription_err] = useState(false)
+    const character_set ={
+        '!':1,
+        '@':1,
+        '#':1,
+        '&':1,
+        '(':1,
+        ')':1,
+        '"':1,
+        "'":1,
+        ' ':1,
+        '-':1
+    }
+
+    const error_string = "Only allowed character set is { a-z , A-Z , 0-9 , ! , @ , # , ( , ) , ' , - " +', " }!'; 
+
+    useEffect(()=>{
+        axios.get(`${basename}/api/language`)
+        .then(res =>{
+            setLanguages(res.data.objects);
+        })
+    },[props])
+
+
     const changeField = (e) =>{
         const field = e.target.name;
         const value = e.target.value;
         setCourseDetails(prev=>{
             return {...prev,[field]:value};
         })
+        if(e.target.name == "name"){
+            let name = e.target.value;
+            if(name)
+            {
+                let f=false;
+                for(let i=0;i<name.length;++i){
+                    if(!((name[i]>='a'&&name[i]<='z')||(name[i]>='A'&&name[i]<='Z')||(name[i]>='0'&&name[i]<='9')||character_set[name[i]])){
+                        f=true;
+                        break;
+                    }
+                }
+                if(f)
+                    setName_err(true);
+                else
+                    setName_err(false);
+            }
+        }
+        if(e.target.name == "description"){
+            let description = e.target.value;
+            if(description)
+            {
+                let f=false;
+                for(let i=0;i<description.length;++i){
+                    if(!((description[i]>='a'&&description[i]<='z')||(description[i]>='A'&&description[i]<='Z')||(description[i]>='0'&&description[i]<='9')||description[i]==','||character_set[description[i]])){
+                        f=true;
+                        break;
+                    }
+                }
+                if(f)
+                    setDescription_err(true);
+                else
+                    setDescription_err(false);
+            }
+        }
+        if(e.target.name == 'cost'){
+            let amount = e.target.value ;
+            if(isNaN(amount)&&amount){
+                setAmount_err(true);
+            }
+            else{
+                setAmount_err(false);
+            }
+        }
+        let start_date = document.getElementById("startDate").value;
+        let end_date = document.getElementById("endDate").value;
+        start_date = Date.parse(start_date);
+        end_date = Date.parse(end_date);
+
+        if(start_date&&end_date&&(end_date<start_date)){
+             setDate_err(true);
+        }
+        else
+            setDate_err(false);
     }
     const [loader,showLoader,hideLoader] = useFullPageLoader();
 
@@ -33,11 +114,15 @@ const CreateCourse = (props) =>{
                 tmp1["startdate"]=tmp.startdate;
                 tmp1["enddate"]=tmp.enddate;
                 tmp1["description"]=tmp.description;
+                tmp1["cost"] = tmp.cost;
                 setCourseDetails(tmp1);
             })
     },[props.match.params['id']])
-    const submitForm = () =>{
+    const submitForm = (e) =>{
+        e.preventDefault();
         const user_id = props.userId;
+        if(amount_err||name_err||description_err||date_err)
+            return;
         axios.get(`${basename}/api/trainer/?user=${user_id}`)
              .then(res1=>{
                  axios.get(`${basename}/api/language/?name=${courseDetails['language']}`)
@@ -75,6 +160,44 @@ const CreateCourse = (props) =>{
              })
         
     }
+
+    let err1,err2,err3,err4;
+    if(amount_err){
+        err1 = (
+            <Alert color="danger" style={{"marginTop":"20px"}}>Please enter valid amount!</Alert>
+        )
+    }
+    else{
+        err1 = (<div></div>)
+    }
+
+    if(name_err){
+        err2 = (
+            <Alert color="danger" style={{"marginTop":"20px"}}>{error_string}</Alert>
+        )
+    }
+    else{
+        err2 = (<div></div>)
+    }
+
+    if(description_err){
+        err3 = (
+        <Alert color="danger" style={{"marginTop":"20px"}}>{error_string}</Alert>
+        )
+    }
+    else{
+        err3 = (<div></div>)
+    }
+
+    if(date_err){
+        err4 = (
+            <Alert color="danger" style={{"marginTop":"20px"}}>Start Date can't be before End Date!</Alert>
+        )
+    }
+    else
+        err4 = (<div></div>);
+
+
     if(redirect){
         return <Redirect to='/dashboard'/>
     }
@@ -84,10 +207,20 @@ const CreateCourse = (props) =>{
                 <FormGroup>
                     <Label for="courseName">Course Name</Label>
                     <Input type="text" name="name" id="courseName" value={courseDetails['name']} bsSize="lg" onChange={changeField}/>
+                    {err2}
                 </FormGroup>
                 <FormGroup>
                     <Label for="language">Language</Label>
-                    <Input type="text" name="language" id="language" value={courseDetails['language']} size="lg" onChange={changeField}/>
+                    <Input type="select" 
+                        onChange={changeField}
+                        name="language" 
+                        value={courseDetails['language']}
+                        size="lg"
+                    >
+                        {languages.map(e=>
+                            (<option value={e.name}>{e.name}</option>)
+                        )}        
+                    </Input>
                 </FormGroup>
                 <FormGroup>
                     <Label for="startDate">Start Date</Label>
@@ -112,10 +245,17 @@ const CreateCourse = (props) =>{
                     onChange={changeField}
                     value={courseDetails['enddate']}
                     />
+                    {err4}
+                </FormGroup>
+                <FormGroup>
+                    <Label for="cost">Purchase Amount</Label>
+                    <Input type="text" name="cost" id="cost" placeholder="Enter the purchase amount" size="lg" onChange={changeField}  value={courseDetails['cost']} required/>
+                    {err1}
                 </FormGroup>
                 <FormGroup>
                     <Label for="description">Course Description</Label>
                     <Input type="textarea" name="description" id="description" bsSize="lg" onChange={changeField} value={courseDetails['description']}/>
+                    {err3}
                 </FormGroup>
                 <Button size="lg" onClick={submitForm}>Submit</Button>
             </Form>
